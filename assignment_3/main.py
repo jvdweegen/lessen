6bfc1c0737d5327b5c30cc6a9e3601734608ba0d
Jamal van der Weegen <202783065+jvdweegen@users.noreply.github.com>
2025-07-16 12:32:18 +0200

    Using os module to load python files without importing them

--- assignment_3/main.py
+++ assignment_3/main.py
@@ -1,14 +1,14 @@
-from aaa import a_print
-from bbb import b_print
-from ccc import c_print
-from ddd import d_print
+import os, sys
 
-def abcd(a_print, b_print, c_print, d_print):
-    a_print
-    b_print
-    c_print
-    d_print
-    return abcd
+def entrypoint():
+    '''
+    Use os.system to load four functions in CWD
+    '''
+    a = os.system("python3 ./aaa.py")
+    b = os.system("python3 ./bbb.py")
+    c = os.system("python3 ./ccc.py") 
+    d = os.system("python3 ./ddd.py")
+    return entrypoint  
 
 if __name__ == "__main__":
-    abcd(a_print, b_print, c_print, d_print)
+    entrypoint()
